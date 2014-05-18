8d7c24f6614e35ae28efb189391983c3299e59bb
Ryan Hiebert <ryan@ryanhiebert.com>
2014-05-18 14:35:55 -0500

    Add tests for PredicateSet

--- tests.py
+++ tests.py
@@ -1,5 +1,59 @@
 import unittest
-from typeset import TypeSet
+from typeset import PredicateSet, TypeSet
+
+
+class PredicateSetTestCase(unittest.TestCase):
+    def test_contains(self):
+        int_set = PredicateSet(lambda x: isinstance(x, int))
+        self.assertTrue(all(x in int_set for x in range(-10, 10)))
+
+    def test_difference(self):
+        a = PredicateSet(lambda x: x in set(range(2)))
+        b = PredicateSet(lambda x: x in set(range(1, 3)))
+
+        self.assertIn(0, a.difference(b))
+        self.assertNotIn(1, a.difference(b))
+        self.assertNotIn(2, a.difference(b))
+
+        self.assertIn(0, a - b)
+        self.assertNotIn(1, a - b)
+        self.assertNotIn(2, a - b)
+
+    def test_intersection(self):
+        a = PredicateSet(lambda x: x in set(range(2)))
+        b = PredicateSet(lambda x: x in set(range(1, 3)))
+
+        self.assertNotIn(0, a.intersection(b))
+        self.assertIn(1, a.intersection(b))
+        self.assertNotIn(2, a.intersection(b))
+
+        self.assertNotIn(0, a & b)
+        self.assertIn(1, a & b)
+        self.assertNotIn(2, a & b)
+
+    def test_symmetric_difference(self):
+        a = PredicateSet(lambda x: x in set(range(2)))
+        b = PredicateSet(lambda x: x in set(range(1, 3)))
+
+        self.assertIn(0, a.symmetric_difference(b))
+        self.assertNotIn(1, a.symmetric_difference(b))
+        self.assertIn(2, a .symmetric_difference(b))
+
+        self.assertIn(0, a ^ b)
+        self.assertNotIn(1, a ^ b)
+        self.assertIn(2, a ^ b)
+
+    def test_union(self):
+        a = PredicateSet(lambda x: x in set(range(2)))
+        b = PredicateSet(lambda x: x in set(range(1, 3)))
+
+        self.assertIn(0, a.union(b))
+        self.assertIn(1, a.union(b))
+        self.assertIn(2, a.union(b))
+
+        self.assertIn(0, a | b)
+        self.assertIn(1, a | b)
+        self.assertIn(2, a | b)
 
 
 class TypeSetTestCase(unittest.TestCase):
